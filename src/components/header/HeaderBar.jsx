d0a3a70477cbba3c8e610acf3490ea0e49a1a1a0
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-08 18:44:52 -0300

    agregando funcionalidad de historial de usuario

--- src/components/header/HeaderBar.jsx
+++ src/components/header/HeaderBar.jsx
@@ -1,18 +1,41 @@
-import React, { useContext, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../contexts/contextProvider";
 import logo from "../../assets/aerolab-logo.svg";
 import coinIcon from "../../assets/icons/coin.svg";
 import AddCoins from "./AddCoins";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faHome,
+    faHistory
+} from '@fortawesome/free-solid-svg-icons';
+
 
 function HeaderBar() {
 
-    const {user: {name, points}} = useContext(Context);
+    const {user: {name, points, redeemHistory},products, setProducts} = useContext(Context);
+    const [showHistory, setHistoryFlag] = useState(false);
+    const [auxProducts, setAuxProducts] = useState([]);
 
     const [flag, setFlag] = useState(false);
 
     const handleAddCoins = () => {
         setFlag(!flag);
     }
+
+    const handleHistory = async () => {
+        setHistoryFlag(!showHistory);
+    }
+
+    useEffect(() => {
+
+        if(products !== redeemHistory) {
+            setAuxProducts(products);
+        }
+
+        setProducts((showHistory ? redeemHistory : auxProducts));
+
+    },[showHistory]);
 
     return(
         <div>
@@ -26,6 +49,7 @@ function HeaderBar() {
                         <span className="cant-coins">{points}</span>
                         <img className="coin-icon" src={coinIcon} alt="coin"/>
                     </button>
+                    <button className="header-button" onClick={handleHistory} title={`see ${name} history`}>{showHistory === false ? <FontAwesomeIcon icon={faHistory} /> : <FontAwesomeIcon icon={faHome} /> }</button> 
                 </div>
             </div>
             {flag === false ? "" : <AddCoins />}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import Filter from "./Filter";
+import Footer from "./Footer";
 import ProductosGallery from "./ProductsGallery";
 import ProductsServices from "../../services/productsServices";
+import { Context } from "../../contexts/contextProvider";
 
 function Main() {
 
-    const [products, setProdcuts] = useState([]);
+    // const [products, setProducts] = useState([]);
 
-    const sortProducts = (sortFunction) => {
-        const sortedProducts = products.slice().sort(sortFunction);
-        setProdcuts(sortedProducts);
-    }
+    const {products, setProducts} = useContext(Context);
 
     const getProducts = async () => {
         const productsArray = await ProductsServices.getProducts();
-        setProdcuts(productsArray);
+        setProducts(productsArray);
     }
 
     useEffect( () => getProducts(), []);
 
     return(
         <div className="main">
-            <Filter sortProducts={sortProducts}/>
+            <Filter products={products} setProducts = {setProducts}/>
             <ProductosGallery products={products} />
+            <Footer />
         </div>
     )
 }; 
