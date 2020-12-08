0874b03d23ebca03d6405c663da40e66a69c3c57
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-08 18:41:39 -0300

    añadiendo estado a los filtros para que queden seleccionados

--- src/components/main/Filter.jsx
+++ src/components/main/Filter.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import chevronLeft from "../../assets/icons/arrow-left.svg";
 import chevronRigth from "../../assets/icons/arrow-right.svg";
+import {params} from "../../services/const";
 
-function Filter({sortProducts}) {
+function Filter({products, setProducts}) {
+
+    const [activeFilter, setActiveFilter] = useState(0);
+
+    const sortProducts = (buttonNumber, sortFunction) => {
+        const sortedProducts = products.slice().sort(sortFunction);
+        setProducts(sortedProducts);
+        setActiveFilter(buttonNumber);
+    }
+
     return(
         <div className="filter-ctn">
             <div className="botonera">
                 <span className="page">16 of 32 products</span>
                 <div className="btns-ctn">
-                    <span className="sort">Sort by:</span>
-                    <button className="button" onClick={()=>sortProducts((p1,p2) => (p1._id<p2._id) ? -1 : 1)}>Most recent</button>
-                    <button className="button" onClick={()=>sortProducts((p1,p2) => p1.cost - p2.cost)}>Lowest price</button>
-                    <button className="button" onClick={()=>sortProducts((p1,p2) => p2.cost - p1.cost)}>Highest price</button>
+                <p className='sort-controls-ctn'>
+                    Sort by:
+                    {params.map(({callback, wording}, i)=> <button key={i} className={`filter-btn ${activeFilter === i ?'active' : ''}`} onClick={()=>sortProducts(i,callback)}>{wording}</button>)}</p>
                 </div>
             </div>
             <div className="move-page">
