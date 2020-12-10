cc9e2cedfd2dc9cf478ca329bb72b962d96e99e5
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-09 22:41:10 -0300

    problema de fecth fixed

--- src/services/productsServices.js
+++ src/services/productsServices.js
@@ -2,32 +2,16 @@ class ProductsServices {
     static async getProducts() {  //llamarlo sin crear un nuevo objeto de la clase
         const headers = {
             "Content-type" : "application/json",
-            Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk"
+            Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmNiYWQ3NjI2ZjdkMjAwMjA0MTE0YzYiLCJpYXQiOjE2MDcxODM3MzR9.8tw4EsTsWF43Ortf_dnCFCfitH96p2hYvqxpKHW5NEU"
         }
         try {
             let response = await fetch("https://coding-challenge-api.aerolab.co/products", {headers} );
-            if(response.status !== 200) {
-                throw new Error("Ups, something went wrong!");
+            if (response.status !== 200) {
+                throw new Error("Ups, something went wrong!")
             }
             response = await response.json();
             return response;
-        } catch (error) {
-            console.log(error);
-        }
-    }
 
-    static async getHistory() {  //llamarlo sin crear un nuevo objeto de la clase
-        const headers = {
-            "Content-type" : "application/json",
-            Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk"
-        }
-        try {
-            let response = await fetch("https://coding-challenge-api.aerolab.co/user/history", {headers} );
-            if(response.status !== 200) {
-                throw new Error("Ups, something went wrong!");
-            }
-            response = await response.json();
-            return response;
         } catch (error) {
             console.log(error);
         }
@@ -36,10 +20,10 @@ class ProductsServices {
     static async postRedeem(productId) {  //llamarlo sin crear un nuevo objeto de la clase
         const headers = {
             "Content-type" : "application/json",
-            Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk"
+            Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmNiYWQ3NjI2ZjdkMjAwMjA0MTE0YzYiLCJpYXQiOjE2MDcxODM3MzR9.8tw4EsTsWF43Ortf_dnCFCfitH96p2hYvqxpKHW5NEU"
         }
 
-        const params = {
+        const parameters = {
             body: {productId},
             method: "post",
             mode: "cors",
@@ -47,12 +31,13 @@ class ProductsServices {
         }
 
         try {
-            let response = await fetch("https://private-anon-c888d91f1b-aerolabchallenge.apiary-mock.com/redeem", {params} );
-            if(response.status !== 200) {
-                throw new Error("Ups, something went wrong!");
+            let response = await fetch("https://private-anon-ebeac586a6-aerolabchallenge.apiary-mock.com/redeem", parameters );
+            if (response.status !== 200) {
+                throw new Error("Ups, something went wrong!")
             }
             response = await response.json();
             return response;
+
         } catch (error) {
             console.log(error);
         }
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -6,8 +6,30 @@ class UserServices {
         }
         try {
             let response = await fetch("https://coding-challenge-api.aerolab.co/user/me", {headers} );
+            if (response.status !== 200) {
+                throw new Error("Ups, something went wrong!")
+            }
             response = await response.json();
             return response;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getHistory() {  //llamarlo sin crear un nuevo objeto de la clase
+        const headers = {
+            "Content-type" : "application/json",
+            Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmNiYWQ3NjI2ZjdkMjAwMjA0MTE0YzYiLCJpYXQiOjE2MDcxODM3MzR9.8tw4EsTsWF43Ortf_dnCFCfitH96p2hYvqxpKHW5NEU"
+        }
+        try {
+            let response = await fetch("https://private-anon-ebeac586a6-aerolabchallenge.apiary-mock.com/user/history", {headers} );
+            if (response.status !== 200) {
+                throw new Error("Ups, something went wrong!")
+            }
+            response = await response.json();
+            return response;
+            
         } catch (error) {
             console.log(error);
         }
@@ -19,7 +41,7 @@ class UserServices {
             Authorization : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmNiYWQ3NjI2ZjdkMjAwMjA0MTE0YzYiLCJpYXQiOjE2MDcxODM3MzR9.8tw4EsTsWF43Ortf_dnCFCfitH96p2hYvqxpKHW5NEU"
         }
 
-        const params = {
+        const parameters = {
             body: {amount: amountCoins},
             method: "post",
             mode: "cors",
@@ -27,9 +49,13 @@ class UserServices {
         }
         
         try {
-            let response = await fetch("https://coding-challenge-api.aerolab.co/user/points", params );
+            let response = await fetch("https://private-anon-ebeac586a6-aerolabchallenge.apiary-mock.com/user/points", parameters );
+            if (response.status !== 200) {
+                throw new Error("Ups, something went wrong!")
+            }
             response = await response.json();
             return response;
+
         } catch (error) {
             console.log(error);
         }
