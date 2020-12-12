444634299bbede10a383d351b4b769882e8e40c6
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-12 20:11:52 -0300

    agregando funcionalidad de paginacion

--- src/utils/Pagination.js
+++ src/utils/Pagination.js
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 
 function usePagination(data, itemsPerPage) {
 
     const [currentPage, setCurrentPage] = useState(1);
     const maxPage = Math.ceil(data.length / itemsPerPage);
+    const maxProducts = data.length;
+    const shownMaxProductsInfo = currentPage * itemsPerPage;
     
-    function currentData() {
+    function currentItems() {
         const begin = (currentPage - 1) * itemsPerPage;
         const end = begin + itemsPerPage;
         return data.slice(begin, end);
     }
     
-    function next() {
+    function nextPage() {
         setCurrentPage((currentPage) => Math.min(currentPage + 1, maxPage));
     }
     
-    function prev() {
+    function prevPage() {
         setCurrentPage((currentPage) => Math.max(currentPage - 1, 1));
     }
+
+    useEffect(() => {
+        setCurrentPage(1);
+    },[data]);
     
-    function jump(page) {
-        const pageNumber = Math.max(1, page);
-        setCurrentPage((currentPage) => Math.min(pageNumber, maxPage));
-    }
-    
-    return { next, prev, jump, currentData, currentPage, maxPage };
+    return { nextPage, prevPage, currentItems, currentPage, maxPage, maxProducts, shownMaxProductsInfo };
 }
 
 export default usePagination;
