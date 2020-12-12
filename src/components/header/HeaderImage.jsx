41b8db3bc750f119c6dfac1bb93df468206b0cf3
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-12 20:16:01 -0300

    agregando por contexto el historial

--- src/components/header/HeaderImage.jsx
+++ src/components/header/HeaderImage.jsx
@@ -4,12 +4,12 @@ import { Context } from "../../contexts/contextProvider";
 
 function HeaderImage() {
     
-    const {user: {redeemHistory},products} = useContext(Context);
+    const { onHistory } = useContext(Context);
 
     return(
         <div className="header-image-ctn">
             <img className="header-image" src={imageHeader} alt="electronics"/>
-            <h1 className="title">{ products.toString() === redeemHistory.toString() ? "Redeem history" : "Electronics"}</h1>
+            <h1 className="title">{ onHistory ? "Redeem history" : "Electronics"}</h1>
         </div>
     )
 }; 
