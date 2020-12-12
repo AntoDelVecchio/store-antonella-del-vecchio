994913be77522d29856740a57777b99f5f98b073
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-12 19:14:35 -0300

    borrando variable que no se usa

--- src/components/main/ProductsGallery.jsx
+++ src/components/main/ProductsGallery.jsx
@@ -4,12 +4,10 @@ import SuccessErrorModal from "./SuccessErrorModal";
 
 function ProductsGallery({products}) {
 
-    let success = true;
-
     return(
         <div className="products-gallery">
             {products.map((product, i) => <ProductCard key={product._id + i} {...product}/> )}
-            <SuccessErrorModal success = {success}/>
+            <SuccessErrorModal />
         </div>
     )
 }; 
