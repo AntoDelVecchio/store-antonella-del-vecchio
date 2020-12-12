f1bbdeedf78b5c53e2fd03de1cdc86ae6f9ed8e3
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-12 13:47:26 -0300

    llamando modal success-error para que sea mostrado cuando sea necesario

--- src/components/main/ProductsGallery.jsx
+++ src/components/main/ProductsGallery.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import SuccessErrorModal from "./SuccessErrorModal";
 
 function ProductsGallery({products}) {
+
+    let success = true;
 
     return(
         <div className="products-gallery">
             {products.map((product, i) => <ProductCard key={product._id + i} {...product}/> )}
+            <SuccessErrorModal success = {success}/>
         </div>
     )
 }; 
