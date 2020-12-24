ebe3a4ad8c9421325549556386ede78a5bc74b6c
AntoDelVecchio <antodelvecchio9@gmail.com>
2020-12-24 14:14:18 -0300

    agregando la comprobacion de filtrar por id o por fecha para fixear el error de los filtros cuando se renderiza el historial; agregando el contenido del alerta para funcion de agregar monedas

--- src/services/const.js
+++ src/services/const.js
@@ -1,7 +1,28 @@
+import { 
+    faSmileWink,
+    faSadTear
+} from '@fortawesome/free-solid-svg-icons';
+
 const params = [
-    {callback: (p1,p2) => (p1._id<p2._id) ? -1 : 1, wording: 'Most recent'},
+    {callback: (p1,p2) => {
+        const attribute = p1.createDate ? "createDate" : "_id"; //comparando propiedad del objeto
+        return (p1[attribute]<p2[attribute]) ? -1 : 1;
+    }, wording: 'Most recent'},
     {callback: (p1,p2) => p1.cost - p2.cost, wording: 'Lowest price'},
     {callback: (p1,p2) => p2.cost - p1.cost, wording: 'Highest price'}
 ];
 
-export {params};
+const coinsAlertMessage = {
+    success: {
+        className: 'success',
+        icon: faSmileWink,
+        message: 'Yay! your coins have been loaded successfully.',
+    },
+    error: {
+        className: 'error',
+        icon: faSadTear,
+        message: 'Oh! something went wrong.',
+    },
+};
+
+export {params, coinsAlertMessage};
